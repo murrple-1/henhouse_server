99f78d17e6f3a8c9021fc3f4476cdb8fde2ac9de
Murray Christopherson <murraychristopherson@gmail.com>
2025-01-25 19:38:34 -0800

    add test

--- art/tests/test_models.py
+++ art/tests/test_models.py
@@ -1,9 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-from art.models import Tag
+from art.models import Tag, Category
 
 
-class TagTestCase(TestCase):
+class CategoryTestCase(SimpleTestCase):
     def test_str(self):
-        tag = Tag.objects.create(pretty_name="Test", name="test")
+        category = Category(pretty_name="Test", name="test")
+        self.assertEqual(str(category), "Category: Test (test)")
+
+
+class TagTestCase(SimpleTestCase):
+    def test_str(self):
+        tag = Tag(pretty_name="Test", name="test")
         self.assertEqual(str(tag), "Tag: Test (test)")
